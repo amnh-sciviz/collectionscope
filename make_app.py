a242572ca1135b702f86d0ef172b5ced27003d48
beefoo <brian@youaremyjoy.org>
2020-03-20 13:42:14 -0400

    implement script that runs all scripts

--- make_app.py
+++ make_app.py
@@ -1,17 +1,35 @@
+# -*- coding: utf-8 -*-
 
+import argparse
+import os
+import subprocess
+import sys
 
+import lib.io_utils as io
 
+# input
+parser = argparse.ArgumentParser()
+parser.add_argument("-config", dest="CONFIG_FILE", default="config-sample.json", help="Config file")
+parser.add_argument("-python", dest="PYTHON_NAME", default="python", help="Name of your python command (e.g. python, python3, etc)")
+parser.add_argument('-clean', dest="CLEAN", action="store_true", help="Start from scratch if app already exists?")
+parser.add_argument('-probe', dest="PROBE", action="store_true", help="Just print commands?")
+a = parser.parse_args()
 
+config = io.readJSON(a.CONFIG_FILE)
+overwriteFlag = ' -overwrite' if a.CLEAN else ''
 
+commands = [
+    '{python_name} scaffold.py -name "{app_name}"{overwrite}'.format(python_name=a.PYTHON_NAME, app_name=config["name"], overwrite=overwriteFlag),
+    '{python_name} prepare_metadata.py -config "{config_file}"'.format(python_name=a.PYTHON_NAME, config_file=a.CONFIG_FILE),
+    '{python_name} prepare_sets.py -config "{config_file}"'.format(python_name=a.PYTHON_NAME, config_file=a.CONFIG_FILE),
+    '{python_name} prepare_positions.py -config "{config_file}"'.format(python_name=a.PYTHON_NAME, config_file=a.CONFIG_FILE),
+    '{python_name} prepare_textures.py -config "{config_file}"'.format(python_name=a.PYTHON_NAME, config_file=a.CONFIG_FILE),
+    '{python_name} prepare_content.py -config "{config_file}"'.format(python_name=a.PYTHON_NAME, config_file=a.CONFIG_FILE)
+]
 
-
-
-# Create scaffolding
-
-# Create metadata
-
-# Create positions
-
-# Create textures
-
-# Create UI
+for command in commands:
+    print('-------------------------------')
+    print(command)
+    if a.PROBE:
+        continue
+    finished = subprocess.check_call(command, shell=True)
